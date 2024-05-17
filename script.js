e8dbb76610beda3630e772bbe721b8e6949d005f
CanataProgrammer <139779715+CanataProgrammer@users.noreply.github.com>
2024-05-17 10:53:52 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+document.getElementById('loginForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('これは教育目的の体験サイトです。実際のAmazonとは関係ありません。');
+});
+
+document.getElementById('signupForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('これは教育目的の体験サイトです。実際のAmazonとは関係ありません。');
+});
